d3801c51094fc9677a5b9f807a3e7d821c62dcfe
Sufyan Mun <Sufyan.zahoor@gmail.com>
2023-01-21 14:41:17 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCIebuctA5dottyGg1-nxDdyT1FuQTtsL0",
+  authDomain: "netflix-clone-fd6ed.firebaseapp.com",
+  projectId: "netflix-clone-fd6ed",
+  storageBucket: "netflix-clone-fd6ed.appspot.com",
+  messagingSenderId: "603712130385",
+  appId: "1:603712130385:web:708c7079da3017c6b49770",
+  measurementId: "G-7603SKYNCS",
+};
