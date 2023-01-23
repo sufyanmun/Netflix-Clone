9a3a3c4069b6377dca30610a0fc46c85777478e6
Sufyan Mun <Sufyan.zahoor@gmail.com>
2023-01-23 17:33:29 -0500

    Row section added

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,14 +2,26 @@ import React from "react";
 import Banner from "./components/Banner";
 import Nav from "./components/Nav";
 import "./HomeScreen.css";
+import request from "./Request";
+import Row from "./Row";
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
       <Banner />
-
-      {/* Row */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={request.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
     </div>
   );
 }
