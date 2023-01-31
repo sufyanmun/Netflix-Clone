958ef6ded357691456abb6afa1c7c1ca9f2a6bd8
Sufyan Mun <Sufyan.zahoor@gmail.com>
2023-01-31 01:42:37 -0500

    Added Profile Screen & changes to login button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 function Nav() {
   const [show, handleShow] = useState(false);
+  const navigate = useNavigate();
 
   const transitionNavBar = () => {
     if (window.scrollY > 100) {
@@ -21,10 +23,12 @@ function Nav() {
     <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__content">
         <img
+          onClick={() => navigate("/")}
           className="nav__logo"
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
         />
         <img
+          onClick={() => navigate("/profile")}
           className="nav__avatar"
           src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
         />
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Nav from "../components/Nav";
+import { selectUser } from "../features/counter/userSlice";
+import { auth } from "./firebase";
 import "./ProfileScreen.css";
 
 function ProfileScreen() {
-  return <div className="profileScreen">ProfileScreen</div>;
+  const user = useSelector(selectUser);
+  return (
+    <div className="profileScreen">
+      <Nav />
+      <div className="profilescreen__body">
+        <h1> Edit Profile</h1>
+        <div className="profileScreen__info">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt=""
+          />
+          <div className="profileScreen__content">
+            <h2> {user.email}</h2>
+            <div className="profileScreen__plans">
+              <button
+                onClick={() => auth.SignOut()}
+                className="profileScreen__signout"
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileScreen;
