79ff178a49b778d359459bc9c96f91cad62ed736
Sufyan Mun <Sufyan.zahoor@gmail.com>
2023-04-06 11:09:18 -0400

    Fixed Logo

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -10,7 +10,7 @@ function LoginScreen() {
       <div className="loginScreen__background">
         <img
           className="loginScreen__logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt="LoginScreen.img"
         />
         <button className="loginScreen__button" onClick={() => setSignIn(true)}>
